46eed2ceb356e6214c82556c867851bbc7e7d3fd
Daniel Kucera <github@danman.eu>
2019-06-24 11:01:02 +0200

    initial commit

---
+++ ymodem.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import sys
+from xmodem import YMODEM
+
+def getc(size, timeout=1):
+    return sys.stdin.read(size) or None
+
+def putc(data, timeout=1):
+    print data
+
+modem = YMODEM(getc, putc)
+
+modem.send([sys.argv[1]])
+
