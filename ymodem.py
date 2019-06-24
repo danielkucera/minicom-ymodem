c4f00ff1cc67c95ba3acc0c180b443ae2888f9ea
Daniel Kucera <daniel.kucera@gmail.com>
2019-06-24 21:00:07 +0200

    show rough sent bytes

--- ymodem.py
+++ ymodem.py
@@ -3,10 +3,24 @@
 import sys
 from xmodem import YMODEM
 
+import logging
+logging.basicConfig()
+
+debug=False
+bytes_sent=0
+
 def getc(size, timeout=1):
-    return sys.stdin.read(size) or None
+    data = sys.stdin.read(size)
+    if debug:
+        sys.stderr.write("read "+repr(data)+"\n")
+    return data or None
 
 def putc(data, timeout=1):
+    global bytes_sent
+    bytes_sent += len(data)
+    sys.stderr.write("sent "+str(bytes_sent/1024)+"kB\r")
+    if debug:
+        sys.stderr.write("write "+str(bytes_sent)+":"+repr(data)+"\n")
     sys.stdout.write(data)
 
 modem = YMODEM(getc, putc)
